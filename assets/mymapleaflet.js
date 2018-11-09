a065b087629a725c3cfb6ffb9e4f442516f58168
Kimberly Miles <kmiles94@uw.edu>
2018-11-08 23:45:03 -0800

    Update mymapleaflet.js

--- assets/mymapleaflet.js
+++ assets/mymapleaflet.js
@@ -3,9 +3,9 @@
 var myData;
 var map = L.map('map').setView([37.560806, -100.763308], 4.25);
 
-var OpenStreetMap_BlackAndWhite = L.tileLayer('https://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+var OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
     maxZoom: 18,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
 
 //add the basemap to the map
